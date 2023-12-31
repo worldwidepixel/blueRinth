ff5e10cbc942937bd204ddb37ceaee71ba06b304
Rowan <104677625+rowandevving@users.noreply.github.com>
2023-12-31 17:26:15 +0000

    add persistent storage + other minor tweaks

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Get all color buttons
     const colorButtons = document.querySelectorAll('.colour');
-
+    
     // Add click event listener to each button
     colorButtons.forEach(button => {
         button.addEventListener('click', function () {
@@ -12,6 +12,11 @@ document.addEventListener('DOMContentLoaded', function () {
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, { action: 'changeStylesheet', stylesheet: buttonId });
             });
+
+            let pref = {};
+            pref["color"] = buttonId;
+            chrome.storage.sync.set(pref); //chrome
+            browser.storage.sync.set(pref); //firefox
         });
     });
 });
