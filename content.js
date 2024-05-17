a4080c30da8bd311a7bb775de4f43ff0e2f73f3f
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-05-16 18:21:46 -0700

    Badger 3.4.0
    Added an easter egg :)

--- content.js
+++ content.js
@@ -10,6 +10,35 @@ chrome.storage.sync.get("color").then(result => {
     changeStylesheet(result["color"]);
 })
 
+document.onreadystatechange = async function () {
+
+    var isEasterEggActive = await chrome.storage.sync.get(["easterEgg"]);
+
+    if (document.readyState == "complete" ) {
+        console.log(await isEasterEggActive['easterEgg']);
+
+        var mrLogoSvg = '';
+
+        fetch(chrome.runtime.getURL(`img/easter_eggs/nineminecraft.svg`))
+            .then(response => response.text())
+            .then((data) => {
+            mrLogoSvg = data;
+        })
+
+        setTimeout(function(){ 
+
+            if (isEasterEggActive['easterEgg'] == true) {
+
+                //document.getElementsByClassName('text-logo')[0].innerHTML = mrLogoSvg;
+                var newLogo = document.createElement('svg');
+                newLogo.innerHTML = mrLogoSvg;
+                document.getElementsByClassName('text-logo')[0].replaceWith(newLogo);
+
+            }
+        }, 200);  
+        
+}};
+
 function changeStylesheet(stylesheet) {
     const existingStyles = document.getElementById('dynamicStylesheet');
         if (existingStyles) {
--- popup.js
+++ popup.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    var easterEggClicks = 0;
+    var logoButton = document.getElementById('logoButton');
+    var easterEggActive = false;
+
+    chrome.storage.sync.get("easterEgg").then(result => {
+        if (result["easterEgg"] == true) {
+            loadEasterEggTrue();
+        } else {
+            loadEasterEggFalse();
+        }
+    })
+
     //Add a check mark to the currently selected color
     chrome.storage.sync.get("color").then(result => {
         const button = document.getElementById(result["color"]);
@@ -18,10 +31,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Get rid of the check mark in the previously selected button
             chrome.storage.sync.get("color").then(result => {
-                const oldButton = document.getElementById(result["color"]);
-                if (oldButton.id != buttonId) {
-                    const child = oldButton.children[0];
-                    child.style.opacity = '0';
+                try {
+                    const oldButton = document.getElementById(result["color"]);
+                    if (oldButton.id != buttonId) {
+                        const child = oldButton.children[0];
+                        child.style.opacity = '0';
+                    }
+                } catch {
+                    
                 }
             });
 
@@ -40,4 +57,66 @@ document.addEventListener('DOMContentLoaded', function () {
             chrome.storage.sync.set(pref);
         });
     });
+
+    logoButton.addEventListener('click', async function () {
+        var isEasterEggActive = await chrome.storage.sync.get(["easterEgg"])
+        console.log(await isEasterEggActive['easterEgg']);
+        if (easterEggClicks < 5) {
+            easterEggClicks++;
+            console.log(easterEggClicks);
+        } else if (easterEggClicks === 5 && await isEasterEggActive['easterEgg'] == false) {
+            logoButton.classList.add("activeEasterEggImg");
+            logoButton.classList.remove("easterEggImg");
+            easterEggClicks = 0;
+            easterEggActive = true;
+            let pref = {};
+            pref["easterEgg"] = easterEggActive;
+            chrome.storage.sync.set(pref);
+
+            if (chrome.storage.sync.get(["color"]) != 'nineMinecraft') {
+                let prevColourPref = {};
+                prevColourPref["prevColour"] = chrome.storage.sync.get(["color"]);
+                chrome.storage.sync.set(prevColourPref);
+            }
+
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { action: 'changeStylesheet', stylesheet: 'nineMinecraft' });
+            });
+
+            let colourPref = {};
+            colourPref["color"] = 'nineMinecraft';
+            chrome.storage.sync.set(colourPref);
+            
+        } else if (easterEggClicks === 5 && await isEasterEggActive['easterEgg'] == true) {
+            logoButton.classList.remove("activeEasterEggImg");
+            logoButton.classList.add("easterEggImg");
+            easterEggClicks = 0;
+            easterEggActive = false;
+            let pref = {};
+            pref["easterEgg"] = easterEggActive;
+            chrome.storage.sync.set(pref);
+
+            var prevColourPref = chrome.storage.sync.get(["prevColour"]);
+
+            let colourPref = {};
+            colourPref["color"] = prevColourPref;
+            chrome.storage.sync.set(colourPref);
+        };
+    });
+
+    function loadEasterEggTrue() {
+        logoButton.classList.add("activeEasterEggImg");
+        logoButton.classList.remove("easterEggImg");
+    }
+
+    function loadEasterEggFalse() {
+        logoButton.classList.remove("activeEasterEggImg");
+        logoButton.classList.add("easterEggImg");
+    }
+
+    async function getEasterEgg() {
+        chrome.storage.sync.get("easterEgg").then(result => {
+            return result["easterEgg"];
+        })
+    }
 });
