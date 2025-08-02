2f76a1c21d3ad184f8b9bd03f798b58206456566
worldwidepixel <worldwidepxl@gmail.com>
2025-08-02 02:35:40 -0700

    feat: blueRinth 4.1: Fix the Fix the Redesign.

---
+++ injected.js
@@ -0,0 +1,80 @@
+document.onreadystatechange = function () {
+  console.log("hi (load)");
+  const projectRegex =
+    /^https?:\/\/modrinth\.com\/(?:project|mod|resourcepack|shader|datapack|modpack)\/([^\/]+)/;
+  const projectIdTest = projectRegex.exec(window.location.href);
+  const projectId =
+    projectIdTest != null && projectIdTest.length > 1 ? projectIdTest[1] : null;
+  if (projectId !== null) {
+    runBanner(projectId);
+    return;
+  }
+  pageSupportsBanners = false;
+};
+
+useNuxtApp()._middleware.global.push(function handleRoute(to, from) {
+  if (to.name.startsWith("type-id")) {
+    const { id } = to.params;
+    //console.log("entering project", id);
+    runBanner(id);
+  } else if (from.name.startsWith("type-id")) {
+    const { id } = from.params;
+    //console.log("leaving project", id);
+    const containers = document.getElementsByClassName("banner-container");
+    for (let container of containers) {
+      document.body.removeChild(container);
+    }
+    pageSupportsBanners = false;
+  }
+});
+
+let pageSupportsBanners = false;
+const fallbackimage =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAnSURBVHhe7cEBDQAAAMKg909tDjcgAAAAAAAAAAAAAAAAAAAA4FwNQEAAAQtzTh0AAAAASUVORK5CYII=";
+let banner = fallbackimage;
+
+async function runBanner(id) {
+  pageSupportsBanners = true;
+  const apibase = "https://api.modrinth.com/v3/project/";
+
+  try {
+    const response = await fetch(apibase + id);
+    if (!response.ok) {
+      console.error("blueRinth: Response failed during banner loading.");
+      return;
+    }
+    const data = await response.json();
+    data.gallery.forEach((entry) => {
+      if (entry.featured === true) {
+        banner = entry.url;
+        applyBanners();
+      }
+    });
+  } catch {
+    console.error(
+      "blueRinth: Something failed during banner loading. Please contact WorldWidePixel."
+    );
+  }
+}
+
+function applyBanners() {
+  if (pageSupportsBanners) {
+    const bannerContainer = document.createElement("div");
+    const bannerStyles = document.createElement("style");
+    bannerStyles.innerHTML = `
+.banner-image {
+  background-image: linear-gradient(0deg, var(--color-bg) 0%, rgba(0, 0, 0, 0) 200%), url(${banner}) !important;
+}
+`;
+    document.head.appendChild(bannerStyles);
+    bannerContainer.classList.add("banner-container");
+    const bannerImage = document.createElement("div");
+    bannerImage.classList.add("banner-image");
+    bannerContainer.appendChild(bannerImage);
+    const bannerBg = document.createElement("div");
+    bannerBg.classList.add("banner-bg");
+    document.body.appendChild(bannerBg);
+    document.body.appendChild(bannerContainer);
+    bannerContainer.style.opacity = "100%";
+  }
+}
--- popup.js
+++ popup.js
@@ -1,78 +1,80 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  var easterEggClicks = 0;
+  var logoButton = document.getElementById("logoButton");
+  var easterEggActive = false;
 
-    var easterEggClicks = 0;
-    var logoButton = document.getElementById('logoButton');
-    var easterEggActive = false;
+  chrome.storage.sync.get("easterEgg").then((result) => {
+    if (result["easterEgg"] == true) {
+      loadEasterEggTrue();
+    } else {
+      loadEasterEggFalse();
+    }
+  });
 
-    chrome.storage.sync.get("easterEgg").then(result => {
-        if (result["easterEgg"] == true) {
-            loadEasterEggTrue();
-        } else {
-            loadEasterEggFalse();
-        }
-    })
+  //Add a check mark to the currently selected color
+  chrome.storage.sync.get("color").then((result) => {
+    const button = document.getElementById(result["color"]);
+    const child = button.children[0];
+    child.style.opacity = "1";
+  });
 
-    //Add a check mark to the currently selected color
-    chrome.storage.sync.get("color").then(result => {
-        const button = document.getElementById(result["color"]);
-        const child = button.children[0];
-        child.style.opacity = '1';
-    })
-    
-    // Get all color buttons
-    const colorButtons = document.querySelectorAll('.colour');
-    
-    // Add click event listener to each button
-    colorButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            
-            // Get the id of the clicked button
-            const buttonId = button.id;
+  // Get all color buttons
+  const colorButtons = document.querySelectorAll(".colour");
 
-            // Get rid of the check mark in the previously selected button
-            chrome.storage.sync.get("color").then(result => {
-                try {
-                    const oldButton = document.getElementById(result["color"]);
-                    if (oldButton.id != buttonId) {
-                        const child = oldButton.children[0];
-                        child.style.opacity = '0';
-                    }
-                } catch {
-                    
-                }
-            });
+  // Add click event listener to each button
+  colorButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      // Get the id of the clicked button
+      const buttonId = button.id;
 
-            // Add the check mark to the clicked button
-            const selectedButton = document.getElementById(buttonId);
-            const child = selectedButton.children[0];
-            child.style.opacity = '1';
+      // Get rid of the check mark in the previously selected button
+      chrome.storage.sync.get("color").then((result) => {
+        try {
+          const oldButton = document.getElementById(result["color"]);
+          if (oldButton.id != buttonId) {
+            const child = oldButton.children[0];
+            child.style.opacity = "0";
+          }
+        } catch {}
+      });
 
-            // Send a message to the content script in the active tab
-            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, { action: 'changeStylesheet', stylesheet: buttonId });
-            });
+      // Add the check mark to the clicked button
+      const selectedButton = document.getElementById(buttonId);
+      const child = selectedButton.children[0];
+      child.style.opacity = "1";
 
-            let pref = {};
-            pref["color"] = buttonId;
-            chrome.storage.sync.set(pref);
+      // Send a message to the content script in the active tab
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          action: "changeStylesheet",
+          stylesheet: buttonId,
         });
+      });
+
+      let pref = {};
+      pref["color"] = buttonId;
+      chrome.storage.sync.set(pref);
     });
+  });
 
-    logoButton.addEventListener('click', async function () {
-        var isEasterEggActive = await chrome.storage.sync.get(["easterEgg"])
-        console.log(await isEasterEggActive['easterEgg']);
-        if (easterEggClicks < 5) {
-            easterEggClicks++;
-            console.log(easterEggClicks);
-        } else if (easterEggClicks === 5 && await isEasterEggActive['easterEgg'] == false) {
-            logoButton.classList.add("activeEasterEggImg");
-            logoButton.classList.remove("easterEggImg");
-            easterEggClicks = 0;
-            easterEggActive = true;
-            let pref = {};
-            pref["easterEgg"] = easterEggActive;
-            chrome.storage.sync.set(pref);
-
+  logoButton.addEventListener("click", async function () {
+    var isEasterEggActive = await chrome.storage.sync.get(["easterEgg"]);
+    console.log(await isEasterEggActive["easterEgg"]);
+    if (easterEggClicks < 5) {
+      easterEggClicks++;
+      //console.log(easterEggClicks);
+    } else if (
+      easterEggClicks === 5 &&
+      (await isEasterEggActive["easterEgg"]) == false
+    ) {
+      logoButton.classList.add("activeEasterEggImg");
+      logoButton.classList.remove("easterEggImg");
+      easterEggClicks = 0;
+      easterEggActive = true;
+      let pref = {};
+      pref["easterEgg"] = easterEggActive;
+      chrome.storage.sync.set(pref);
+      /*
             if (chrome.storage.sync.get(["color"]) != 'nineMinecraft') {
                 let prevColourPref = {};
                 prevColourPref["prevColour"] = chrome.storage.sync.get(["color"]);
@@ -85,38 +87,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
             let colourPref = {};
             colourPref["color"] = 'nineMinecraft';
-            chrome.storage.sync.set(colourPref);
-            
-        } else if (easterEggClicks === 5 && await isEasterEggActive['easterEgg'] == true) {
-            logoButton.classList.remove("activeEasterEggImg");
-            logoButton.classList.add("easterEggImg");
-            easterEggClicks = 0;
-            easterEggActive = false;
-            let pref = {};
-            pref["easterEgg"] = easterEggActive;
-            chrome.storage.sync.set(pref);
-
+            chrome.storage.sync.set(colourPref);*/
+    } else if (
+      easterEggClicks === 5 &&
+      (await isEasterEggActive["easterEgg"]) == true
+    ) {
+      logoButton.classList.remove("activeEasterEggImg");
+      logoButton.classList.add("easterEggImg");
+      easterEggClicks = 0;
+      easterEggActive = false;
+      let pref = {};
+      pref["easterEgg"] = easterEggActive;
+      chrome.storage.sync.set(pref);
+      /*
             var prevColourPref = chrome.storage.sync.get(["prevColour"]);
 
             let colourPref = {};
             colourPref["color"] = prevColourPref;
-            chrome.storage.sync.set(colourPref);
-        };
+            chrome.storage.sync.set(colourPref);*/
+    }
+  });
+
+  function loadEasterEggTrue() {
+    logoButton.classList.add("activeEasterEggImg");
+    logoButton.classList.remove("easterEggImg");
+  }
+
+  function loadEasterEggFalse() {
+    logoButton.classList.remove("activeEasterEggImg");
+    logoButton.classList.add("easterEggImg");
+  }
+
+  async function getEasterEgg() {
+    chrome.storage.sync.get("easterEgg").then((result) => {
+      return result["easterEgg"];
     });
-
-    function loadEasterEggTrue() {
-        logoButton.classList.add("activeEasterEggImg");
-        logoButton.classList.remove("easterEggImg");
-    }
-
-    function loadEasterEggFalse() {
-        logoButton.classList.remove("activeEasterEggImg");
-        logoButton.classList.add("easterEggImg");
-    }
-
-    async function getEasterEgg() {
-        chrome.storage.sync.get("easterEgg").then(result => {
-            return result["easterEgg"];
-        })
-    }
+  }
 });
